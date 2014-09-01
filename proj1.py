166dd73bd188e2cbb223fe70e52a06132b69adfd
validum <eric_wu2001@yahoo.com>
2014-08-31 21:23:46 -0400

    Final

--- proj1.py
+++ proj1.py
@@ -7,15 +7,10 @@ Created on Sun Aug 31 11:32:05 2014
 
 EX_GRAPH0 = {0:set([1,2]), 1:set([]), 2:set([])}
 EX_GRAPH1 = {0:set([1,4,5]), 1:set([2,6]), 2:set([3]), 3:set([0]), 4:set([1]), 5:set([2]), 6:set([])}
-EX_GRAPH2 = {0:set([1,4,5]), 1:set([2,6]), 2:set([3,7]), 3:set([7]), 4:set([1]), 5:set([2]), 7:set([3]), 8:set([1,2]), 9:set([0,3,4,5,6,7])}
-
-
-#print EX_GRAPH0
-#print EX_GRAPH1
-#print EX_GRAPH2
-
+EX_GRAPH2 = {0:set([1,4,5]), 1:set([2,6]), 2:set([3,7]), 3:set([7]), 4:set([1]), 5:set([2]), 6:set([]), 7:set([3]), 8:set([1,2]), 9:set([0,3,4,5,6,7])}
 
 def make_complete_graph(num_nodes):
+    "creates dictionary with all possible edges, no self loop"
     complete_digraph = {}
     for node in range(num_nodes):
         lst = range(num_nodes)
@@ -25,8 +20,8 @@ def make_complete_graph(num_nodes):
         
     
 def compute_in_degrees(digraph):
+    "computes ind-degrees for a digraph represented as a dictionary"
     indegree_dict = digraph.copy()
-    print indegree_dict
     for key in indegree_dict.keys():
         indegree_dict[key] = 0
     for key in digraph.keys():
@@ -36,38 +31,15 @@ def compute_in_degrees(digraph):
     return indegree_dict
 
 
+def in_degree_distribution(digraph):
+    "takes digraph represented as dictionary and returns unnormalized in-degree distribution"
+    degree_dist = {}
+    digraph_in_degrees = compute_in_degrees(digraph)
+    for key in digraph_in_degrees.keys():
+        num_nodes = digraph_in_degrees[key]
+        if num_nodes in degree_dist:
+            degree_dist[num_nodes] = degree_dist[num_nodes] + 1
+        else:
+            degree_dist[num_nodes] = 1
+    return degree_dist
 
-#kkk = compute_in_degrees(EX_GRAPH1)
-#
-#print kkk
-
-#j=10
-#print j
-#
-#list = range(j)
-#list.remove(5)
-#print list
-#
-#dict = {}
-#dict[3] = set(list)
-#print dict
-#
-#bbb = make_complete_graph(5)
-#
-#print bbb
-
-#testdict = EX_GRAPH1.copy() 
-#
-#
-#print testdict
-#
-#for key in testdict.keys():
-#    testdict[key] = 0
-#    testdict[key] = testdict[key]+1
-#    
-#print testdict
-#print EX_GRAPH1
-#for key in EX_GRAPH1.keys():
-#    j = EX_GRAPH1[key]
-#    for ind in j:
-#        print ind
